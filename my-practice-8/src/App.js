c8a57a6573d56017bcb2785956fa89c59736bf02
Nhoj <nhojplummer@yahoo.com>
2023-04-14 15:15:00 -0400

    Connected component to app.js

--- my-practice-8/src/App.js
+++ my-practice-8/src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import "./App.css"
+import Display from "./components/Display"
 
 function App() {
   return (
     <div className="App">
-    
+      <Display />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ my-practice-8/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Display() {
+  return (
+    <div>Display</div>
+  )
+}
+
+export default Display
